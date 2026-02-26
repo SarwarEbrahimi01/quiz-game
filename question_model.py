8625d3bd53228c054a5bc6485b6c85d85afd831a
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-26 09:28:03 +0430

    Create question_model.py

---
+++ question_model.py
@@ -0,0 +1,9 @@
+# Part 1: Create a Question class with an __init()__ method  with two attributes: text and answer attribute.
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+# new_q = Question("How old are you?", "I am 21 years old.")
